c69159d4d8e9affc8abc8aa8e24ef04db66b7ae2
zlongin <78811047+zlongin@users.noreply.github.com>
2022-03-20 17:36:29 +0000

    no clue what to do, whats going on, no project idea, no maidens, shame

--- main.py
+++ main.py
@@ -1,79 +1,52 @@
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-
 import pandas as pd
 import numpy as np
 
-footballdata = pd.read_csv("C:\Users\w19023479\Downloads\archive\spreadspoke_scores.csv")
-footballdata.head()
-footballdata_features = footballdata.copy()
-footballdata_homelabel = footballdata_features.pop('score_home');
-footballdata_awaylabel = footballdata_features.pop('score_away');
+#i have no clue what to do get me out ASAP
 
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
 
-inputs = {}
-
-for name, column in footballdata_features.items():
-    dtype = column.dtype
-    if dtype == object:
-        dtype = tf.string
-    else
-        dtype = tf.float32
-    inputs[name] = tf.keras.Input(shape=(1,), name=name, dtype=dtype)
-
-numeric_inputs = {name:input for name,input in inputs.items()
-                  if input.dtype==tf.float32}
-
-x = layers.Concatenate()(list(numeric_inputs.values()))
-norm = layers.Normalization()
-norm.adapt(np.array(footballdata[numeric_inputs.keys()]))
-all_numeric_inputs = norm(x)
-
-preprocessed_inputs = [all_numeric_inputs]
-
-for name, input in inputs.items():
-  if input.dtype == tf.float32:
-    continue
-
-  lookup = layers.StringLookup(vocabulary=np.unique(footballdata_features[name]))
-  one_hot = layers.CategoryEncoding(num_tokens=lookup.vocabulary_size())
-
-  x = lookup(input)
-  x = one_hot(x)
-  preprocessed_inputs.append(x)
-  
-preprocessed_inputs_cat = layers.Concatenate()(preprocessed_inputs)
-
-footballdata_preprocessing = tf.keras.Model(inputs, preprocessed_inputs_cat)
-
-tf.keras.utils.plot_model(model = footballdata_preprocessing , rankdir="LR", dpi=72, show_shapes=True)
-
-footballdata_features_dict = {name: np.array(value) 
-                         for name, value in footballdata_features.items()}
+import tensorflow as tf
+from tensorflow.keras import layers
                          
-features_dict = {name:values[:1] for name, values in footballdata_features_dict.items()}
-footballdata_preprocessing(features_dict)
 
-def footballdatamodel(preprocessing_head, inputs):
-    body = tf.keras.Sequential([
-        layers.Dense(64),
-        layers.Dense(1)
-        ])
-    preprocessed_inputs = preprocessing_head(inputs)
-    result = body(preprocessed_inputs)
-    model = tf.keras.Model(inputs, result)
-    model.compile(loss=tf.losses.BinaryCrossentropy(from_logits=True),
-    optimizer=tf.optimizers.Adam())
-    
-    return model
-footballdatamodel = footballdatamodel = (footballdata_preprocessing, inputs)
+losses_train = pd.read_csv(
+    "C:\\Users\\zacha\\Desktop\\AI\\russia_losses_equipment.csv",
+    names=["day", "aircraft", "helicoptor", "tank", "APC",
+           "field artillery", "MRL", "military auto", "fuel tank", "drone", "naval ship", "anti-aircraft warefare"],
+    skiprows=1)
 
-footballdatamodel.fit(x = footballdata_features_dict, y =footballdata_homelabel, epochs = 10)
+losses_train.head()
+
+losses_features = losses_train.copy()
+
+losses_labels = losses_features.pop('aircraft')
+losses_labels = losses_features.pop('helicoptor')
+losses_labels = losses_features.pop('tank')
+losses_labels = losses_features.pop('APC')
+losses_labels = losses_features.pop('field artillery')
+losses_labels = losses_features.pop('MRL')
+losses_labels = losses_features.pop('military auto')
+losses_labels = losses_features.pop('fuel tank')
+losses_labels = losses_features.pop('drone')
+losses_labels = losses_features.pop('naval ship')
+losses_labels = losses_features.pop('anti-aircraft warefare')
+
+losses_features = np.array(losses_features)
+
+losses_model = tf.keras.Sequential([
+  layers.Dense(64),
+  layers.Dense(1)
+])
+
+print(losses_features)
+
+losses_model.compile(loss = tf.keras.losses.MeanSquaredError(),
+                      optimizer = tf.optimizers.Adam())
+
+losses_model.fit(losses_features, losses_labels, epochs=10)
 
 
-
-
-                         
 
 
 
